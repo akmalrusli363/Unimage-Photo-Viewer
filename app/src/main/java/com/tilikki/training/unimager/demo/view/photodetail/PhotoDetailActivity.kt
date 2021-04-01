186da25fe0f19ddea43cb477a6931fbeffbc1939
Akmal <akmalrusli363@outlook.com>
2021-04-01 20:14:39 +0700

    Added photo detail activity

---
+++ app/src/main/java/com/tilikki/training/unimager/demo/view/photodetail/PhotoDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.tilikki.training.unimager.demo.view.photodetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tilikki.training.unimager.demo.R
+
+class PhotoDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_detail)
+    }
+}
