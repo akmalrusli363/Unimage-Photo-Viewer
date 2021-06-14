7f720b6c0b70d900a20400d8c7a462a98d202cd0
Akmal <akmalrusli363@outlook.com>
2021-06-14 21:27:49 +0700

    Fixed Android Injector base URL & removed unused modules

--- app/src/main/java/com/tilikki/training/unimager/demo/core/UnimageApplication.kt
+++ app/src/main/java/com/tilikki/training/unimager/demo/core/UnimageApplication.kt
@@ -1,6 +1,7 @@
 package com.tilikki.training.unimager.demo.core
 
 import com.tilikki.training.unimager.demo.injector.component.DaggerAppComponent
+import com.tilikki.training.unimager.demo.repositories.UnsplashRepository
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
@@ -8,6 +9,7 @@ class UnimageApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder()
             .application(this)
+            .baseUrl(UnsplashRepository.BASE_URL)
             .build()
     }
 }
