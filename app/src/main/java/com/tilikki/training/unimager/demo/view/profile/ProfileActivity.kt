b04f47a734cb96c939ec77e9b6ef86b357a6acdb
Akmal <akmalrusli363@outlook.com>
2023-09-08 17:05:44 +0700

    Migrated user profile screen to Compose

--- app/src/main/java/com/tilikki/training/unimager/demo/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/tilikki/training/unimager/demo/view/profile/ProfileActivity.kt
@@ -1,14 +1,11 @@
 package com.tilikki.training.unimager.demo.view.profile
 
 import android.os.Bundle
-import android.view.MenuItem
-import com.tilikki.training.unimager.demo.R
-import com.tilikki.training.unimager.demo.databinding.ActivityProfileBinding
-import com.tilikki.training.unimager.demo.model.User
-import com.tilikki.training.unimager.demo.util.ImageLoader
-import com.tilikki.training.unimager.demo.util.ViewUtility
-import com.tilikki.training.unimager.demo.util.value
-import com.tilikki.training.unimager.demo.view.photogrid.PhotoGridFragment
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.tilikki.training.unimager.demo.view.compose.ErrorScreen
+import com.tilikki.training.unimager.demo.view.compose.LoadingIndicator
 import dagger.android.AndroidInjection
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
@@ -17,7 +14,6 @@ class ProfileActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var viewModel: ProfileViewModel
 
-    private lateinit var binding: ActivityProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -31,78 +27,31 @@ class ProfileActivity : DaggerAppCompatActivity() {
             finish()
         }
 
-        binding = ActivityProfileBinding.inflate(layoutInflater)
-        binding.lifecycleOwner = this
-
-        setContentView(binding.root)
-
         viewModel.fetchUserProfile(username!!)
-        viewModel.userProfile.observe(this, {
-            bindUserProfile(it)
-        })
-        viewModel.userPhotos.observe(this, {
-            supportFragmentManager.beginTransaction()
-                .replace(binding.fragmentPhotosGrid.id, PhotoGridFragment.newInstance(it))
-                .commit()
-        })
-        viewModel.isFetching.observe(this, {
-            ViewUtility.setVisibility(binding.pbLoading, it)
-            if (it) {
-                ViewUtility.setVisibility(binding.nsvPage, false)
-                ViewUtility.setVisibility(binding.llError, false)
+        setContent {
+            val photos by viewModel.userPhotos.observeAsState()
+            val user by viewModel.userProfile.observeAsState()
+            val fetching by viewModel.isFetching.observeAsState()
+            val success by viewModel.successResponse.observeAsState()
+            if (fetching == true) {
+                LoadingIndicator()
+            } else {
+                if (success?.success == true && user != null) {
+                    ProfileView(user = user!!, photos = photos)
+                } else {
+                    ErrorScreen()
+                }
             }
-        })
-        viewModel.successResponse.observe(this, {
-            it.observeResponseStatus({
-                toggleDataState(true)
-            }, {
-                toggleDataState(false)
-            })
-        })
-    }
 
-    private fun bindUserProfile(user: User) {
-        binding.apply {
-            tvUsername.text = user.username
-            tvFullName.text = user.name
-            tvPhotos.text = ViewUtility.displayPluralText(
-                resources,
-                R.plurals.total_photos_format,
-                user.totalPhotos,
-            )
-            tvFollowers.text = ViewUtility.displayPluralText(
-                resources,
-                R.plurals.followers_format,
-                user.followers.value(),
-            )
-            tvFollowing.text = ViewUtility.displayPluralText(
-                resources,
-                R.plurals.following_format,
-                user.following.value()
-            )
-            ImageLoader.loadImage(user.profileImageUrl, ivProfileImage)
-            ivProfileImage.contentDescription = getUserAvatarDescription(user)
         }
-        this@ProfileActivity.title = "${user.name} Profile"
-    }
-
-    private fun getUserAvatarDescription(user: User): String {
-        return getString(R.string.user_avatar_description, user.username)
     }
 
     private fun getFromIntent(): String? {
         return intent.getStringExtra(INTENT_URL)
     }
 
-    private fun toggleDataState(success: Boolean) {
-        ViewUtility.toggleVisibilityPairs(binding.nsvPage, binding.llError, success)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> onBackPressed()
-            else -> super.onOptionsItemSelected(item)
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
         return true
     }
 
---
+++ app/src/main/java/com/tilikki/training/unimager/demo/view/profile/ProfileView.kt
@@ -0,0 +1,123 @@
+package com.tilikki.training.unimager.demo.view.profile
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Divider
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.tilikki.training.unimager.demo.R
+import com.tilikki.training.unimager.demo.model.Photo
+import com.tilikki.training.unimager.demo.model.User
+import com.tilikki.training.unimager.demo.ui.theme.SizeUnit
+import com.tilikki.training.unimager.demo.util.SampleComposePreviewData
+import com.tilikki.training.unimager.demo.util.value
+import com.tilikki.training.unimager.demo.view.compose.PreviewEmptyScreen
+import com.tilikki.training.unimager.demo.view.photogrid.PhotoGrid
+
+@Composable
+fun ProfileView(user: User, photos: List<Photo>?) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (!photos.isNullOrEmpty()) {
+            PhotoGrid(photos) {
+                UserOverview(user = user)
+                Divider(modifier = Modifier.padding(SizeUnit.SPACE_SMALL), color = Color.Gray)
+            }
+        } else {
+            PreviewEmptyScreen()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProfileView() {
+    val user = SampleComposePreviewData.generateSampleUserData("username")
+    val photos = (0..10).map { SampleComposePreviewData.generateSamplePhotoData("photo$it") }
+    ProfileView(user = user, photos = photos)
+}
+
+@Composable
+fun UserOverview(user: User) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(SizeUnit.SPACE_MEDIUM)
+            .fillMaxWidth()
+    ) {
+        AsyncImage(
+            model = user.profileImageUrl,
+            placeholder = painterResource(id = R.drawable.ic_person),
+            contentDescription = stringResource(
+                R.string.username_format, user.name, user.username
+            ),
+            modifier = Modifier
+                .padding(SizeUnit.SPACE_SMALL)
+                .width(64.dp)
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.Gray)
+        )
+        Text(
+            user.name,
+            style = MaterialTheme.typography.h6,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            user.username,
+            style = MaterialTheme.typography.subtitle1
+        )
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Row(horizontalArrangement = Arrangement.SpaceAround) {
+                Text(
+                    pluralStringResource(
+                        id = R.plurals.total_photos_format,
+                        count = user.totalPhotos, user.totalPhotos
+                    ),
+                    modifier = Modifier.padding(SizeUnit.SPACE_MEDIUM),
+                )
+                Text(
+                    pluralStringResource(
+                        id = R.plurals.following_format,
+                        count = user.following.value(), user.following.value()
+                    ), modifier = Modifier.padding(SizeUnit.SPACE_MEDIUM)
+                )
+                Text(
+                    pluralStringResource(
+                        id = R.plurals.followers_format,
+                        count = user.followers.value(), user.followers.value()
+                    ), modifier = Modifier.padding(SizeUnit.SPACE_MEDIUM)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewUserOverview() {
+    UserOverview(user = SampleComposePreviewData.generateSampleUserData("username"))
+}
