7329084bb1e6aceec68b492bc0ab87734096d7d8
Akmal <akmalrusli363@outlook.com>
2023-09-08 17:03:24 +0700

    Fixed field properties text color shades compared to original XML Material typography style

--- app/src/main/java/com/tilikki/training/unimager/demo/view/compose/BaseUiKit.kt
+++ app/src/main/java/com/tilikki/training/unimager/demo/view/compose/BaseUiKit.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -94,13 +97,15 @@ fun ParameterField(field: String, value: Any?) {
                 fontWeight = FontWeight.Medium,
                 style = MaterialTheme.typography.body1
             )
-            Text(
-                text = value.toString(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(SizeUnit.SPACE_SMALL),
-                textAlign = TextAlign.End,
-            )
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(
+                    text = value.toString(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(SizeUnit.SPACE_SMALL),
+                    textAlign = TextAlign.End,
+                )
+            }
         }
     }
 }
